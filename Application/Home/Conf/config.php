e7844fc87a9a8f636095e5ecda83c56acd40b4f8
sxlifeng <sxlifeng@126.com>
2014-11-02 18:49:16 +0800

    修改数据库连接

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 	//'配置项'=>'配置值'
 	/* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '10.4.12.173', // 服务器地址
-    'DB_NAME'               =>  'd722c5b44f5734dbfb102d964dfdb3417',          // 数据库名
-    'DB_USER'               =>  'uRG7ilLCgPYW4',      // 用户名
-    'DB_PWD'                =>  'pYd9dLbgb9xOO',          // 密码
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'cattedu',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'tb_',    // 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
